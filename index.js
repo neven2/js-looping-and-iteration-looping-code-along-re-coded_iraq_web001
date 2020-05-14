edb4c9ebe8aec3fb7f27e911ea7c7a6af8bcdf2c
Neven Bedr <neven2225@gmail.com>
2020-05-14 20:13:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 
-const names = ["Liza", "Kaitlin", "Jan"];
+const names = ["Lisa", "Kaitlin", "Jan"];
 const events = ["surprise"];
 function writeCards(names,events){
  let greeting = []
