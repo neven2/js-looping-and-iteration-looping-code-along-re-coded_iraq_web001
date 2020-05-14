393bebfd227d07d7f0a7084ba83fa9e346850436
Neven Bedr <neven2225@gmail.com>
2020-05-14 20:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function writeCards(arr,string2){
  let array = []
   for (let i = 0; i < arr.length; i++){
     array.push("Thank you " +arr[i]+",for the wonderful"+string2+"gift!")
+     array.push(`Thank you, ${arr[i]}, for the wonderful ${string2} gift!`);
   }
   return array;
 }
