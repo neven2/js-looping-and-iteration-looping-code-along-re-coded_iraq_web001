4ef630ccc06aeb14af0eaea7e311787d68dd31c1
Neven Bedr <neven2225@gmail.com>
2020-05-14 20:06:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const arr=["Lisa", "Kaitlin", "Jan"];
+function writeCards(arr,string1){
+  const array=[];
+for(let i=0;i<arr.length;i++){
+
+  array.push("Thak you, "+arr[i]+", for the wonderful "+string1+" gift!");
+ 
+}
+return array;
+}
+
+
+
+function countDown(num){
+  let i=num;
+  while(i>=0){
+    console.log(i);
+    i--;
+  }
+}
+countDown(10);
