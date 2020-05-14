c3e971c47225e4707795fd8dfa344111d513a5de
Neven Bedr <neven2225@gmail.com>
2020-05-14 20:18:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const string2 = ["surprise"];
 function writeCards(arr,string2){
  let array = []
   for (let i = 0; i < arr.length; i++){
+    array.push("Thank you " +arr[i]+",for the wonderful"+string2+"gift!")
      array.push(`Thank you, ${arr[i]}, for the wonderful ${string2} gift!`);
   }
   return array;
